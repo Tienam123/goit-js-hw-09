c9912e49d77d4c5150ed68d3628fba6cbc73e309
Tienam123 <123368743+Tienam123@users.noreply.github.com>
2024-02-10 07:29:07 +0000

    Deploying to gh-pages from @ Tienam123/goit-js-hw-09@9d5a12e323c8df9629f2f5e124f6492158b0c8b7 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,10 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const a=[{name:[`<ul>
+        <li>Оголошена змінна customer</li>
+        <li>Код отформатирован Prettier</li>
+        <li><a href="./1-gallery.html">Перейти на страницу выполнения задания</a></li>
+      </ul>`]},{name:[`<ul>
+        <li>Створи клас Storage, який створюватиме об'єкти для управління складом товарів.Клас очікує лише один аргумент — початковий масив товарів який записується до створеного об'єкта в приватну властивість items.</li>
+        <li>getItems() — повертає масив поточних товарів у приватній властивості items.</li>
+        <li><a href="./2-form.html">Перейти на страницу выполнения задания</a></li>
+        </ul>`]}],e=document.querySelectorAll(".btn-resources"),i=document.querySelector("#tab-content");e.forEach(t=>t.addEventListener("click",r));function r(t){e.forEach((l,n)=>{l.classList.remove("active")}),t.target.classList.add("active"),i.innerHTML=a[t.target.dataset.number].name}
+//# sourceMappingURL=commonHelpers3.js.map
